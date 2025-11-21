857702b487b97a01bc80020ee910c945b1a2e2af
haoshiliang <haoshiliang@migu.cn>
2025-11-21 12:02:51 +0800

    remmove test file“

--- test_basic.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    std::cout << "Basic test successful!" << std::endl;
-    return 0;
-}
--- test_simple.cpp
+++
@@ -1,7 +0,0 @@
-#include "Inc/ICYLoggerDefine.hpp"
-#include <iostream>
-
-int main() {
-    std::cout << "CYLogger test" << std::endl;
-    return 0;
-}
