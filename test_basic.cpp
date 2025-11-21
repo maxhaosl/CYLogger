8c50cc59ab2b3305fd3b30a5e0b0bc29ee17cf1d
haoshiliang <haoshiliang@migu.cn>
2025-11-21 11:59:49 +0800

    add test file

---
+++ test_basic.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Basic test successful!" << std::endl;
+    return 0;
+}
---
+++ test_simple.cpp
@@ -0,0 +1,7 @@
+#include "Inc/ICYLoggerDefine.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "CYLogger test" << std::endl;
+    return 0;
+}
