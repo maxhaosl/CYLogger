6bab630c597df811021c422f1c88ae007918b033
haoshiliang <haoshiliang@migu.cn>
2025-11-25 15:20:43 +0800

    Add test cases and solve the problem of cross-platform coroutine stuck

---
+++ Build/MacOS/CYLoggerTest/CYLoggerTest/main.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  CYLoggerTest
+//
+//  Created by 时亮郝 on 2025/11/25.
+//
+
+#include <iostream>
+#include "ICYLogger.hpp"
+#ifdef CYLOGGER_WIN_OS
+#include <windows.h>
+#include <tchar.h>
+#define TCHAR TCHAR
+#else
+#include <cstring>
+#include <unistd.h>
+#define MAX_PATH 260
+#define GetModuleFileName(A, B, C) (getcwd((char*)(B), (C)))
+#define _tcsrchr strchr
+#define _T(x) x
+#define TCHAR char
+#endif
+
+int main(int argc, const char * argv[]) {
+    TCHAR szFilePath[MAX_PATH] = { 0 };
+    GetModuleFileName(nullptr, szFilePath, sizeof(szFilePath));
+    char* lastSlash = _tcsrchr(szFilePath, '/');
+    if (lastSlash) lastSlash[1] = 0;
+
+    lastSlash = _tcsrchr(szFilePath, '\\');
+    if (lastSlash) lastSlash[1] = 0;
+
+    CY_LOG_CONFIG(szFilePath, LOG_SHOW_CONSOLE_WINDOW, LOG_LAYOUT_TYPE);
+
+    char szBuffer[256] = { 1 };
+
+    int nLineCount = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        CY_LOG_TRACE("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_DEBUG("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_INFO("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_WARN("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_ERROR("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_FATAL("%d - %s", nLineCount, "This is a test message!");
+
+        CY_LOG_ESCAPE_TRACE("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_ESCAPE_DEBUG("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_ESCAPE_INFO("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_ESCAPE_WARN("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_ESCAPE_ERROR("%d - %s", nLineCount, "This is a test message!");
+        CY_LOG_ESCAPE_FATAL("%d - %s", nLineCount, "This is a test message!");
+
+        CY_LOG_DIRECT_TRACE("This is a test message!");
+        CY_LOG_HEX_TRACE(szBuffer, sizeof(szBuffer));
+    }
+
+    CY_LOG_FREE();
+    return EXIT_SUCCESS;
+}
